81c5df81ef6b5f6fda019ae223e8dece34fa025a
Vendelín <vendelinkukla@gmail.com>
2022-02-21 11:35:56 +0100

    3 commit

--- Opakovani/App.cs
+++ Opakovani/App.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Opakovani
 {
@@ -10,7 +11,8 @@ namespace Opakovani
     {
         public void Run()
         {
-
+            DataLoader load = new DataLoader();
+            load.Load();
         }
     }
 }
---
+++ Opakovani/DataLoader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Opakovani
+{
+    class DataLoader
+    {
+        public void Load()
+        {
+            string[] lines = File.ReadAllLines("./names.txt");
+            foreach (string line in lines)
+                Console.WriteLine(line);
+        }
+    }
+}
